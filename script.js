f75dfac2c8eacf5415a5582e14f42899bac1ce52
annamanoelly <anna.mannuuh73@gmail.com>
2024-05-29 20:21:08 -0300

    mudei o menu e deixei responsivo

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(funcionando)
+
+
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/menu.svg";
+    }
+}
